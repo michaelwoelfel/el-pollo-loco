38599c000019a688e360265be21e74f9570fa6c1
Michel <woelfelm@t-online.de>
2023-08-20 11:10:51 +0200

    fullscreen

--- js/game.js
+++ js/game.js
@@ -65,6 +65,36 @@ function muteSound() {
 }
 
 
+function fullscreen() {
+    let fullscreen = document.getElementById('fullscreen');
+openFullscreen(fullscreen);
+}
+
+
+
+
+
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
 
 
 
